80aa39fcb9a8d45cd4f923a43c77452bac34ada4
MatiS000 <matyldasojka@gmail.com>
2024-05-23 20:13:10 +0200

    Dni do końca miesiąca
    W tym commicie dodano funkcjonalność obliczająca dni do końca miesiąca.

--- main.c
+++ main.c
@@ -8,11 +8,38 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 
+unsigned int DaysToMonthEnd(unsigned int day, unsigned int month)
+{
+    unsigned int daysToMonthEnd;
+    unsigned int daysInMonth;
+    switch(month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            daysInMonth = 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            daysInMonth = 30;
+            break;
+        case 2:
+            daysInMonth = 28;
+            break;
+    }
+    
+    return daysToMonthEnd =  daysInMonth - day;
+}
+
+
 int main()
 {
-unsigned int day;
-unsigned int month;
-unsigned int year;
-
+    printf("Dni do konca miesiaca: %d", DaysToMonthEnd(6, 2));
 	return 0;
 }
